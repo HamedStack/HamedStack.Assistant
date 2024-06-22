6b39534d46e473c53da00c78bf39c1bdd75733e2
hamedfathi <hamedfathi@outlook.com>
2024-06-22 12:36:54 +0200

    feat(Utilities): UUIDv7 and StaTask added.

---
+++ HamedStack.Assistant/Utilities/StaTask.cs
@@ -0,0 +1,54 @@
+﻿namespace HamedStack.Assistant.Utilities;
+
+/// <summary>
+/// Provides methods to run tasks asynchronously on a single-threaded apartment (STA) thread.
+/// </summary>
+public static class StaTask
+{
+    /// <summary>
+    /// Runs the specified action asynchronously on an STA thread.
+    /// </summary>
+    /// <param name="action">The action to execute.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    /// <remarks>
+    /// This method is useful for running actions that require STA thread, such as interacting with COM objects
+    /// or Windows Forms controls.
+    /// </remarks>
+    public static Task RunAsync(Action action) =>
+        RunAsync(_ => action(), CancellationToken.None);
+
+    /// <summary>
+    /// Runs the specified action asynchronously on an STA thread, supporting cancellation.
+    /// </summary>
+    /// <param name="action">The action to execute. The action takes a <see cref="CancellationToken"/> that is signaled when the operation is cancelled.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    /// <remarks>
+    /// This method is useful for running actions that require STA thread, such as interacting with COM objects
+    /// or Windows Forms controls. The action can be cancelled by signalling the provided <see cref="CancellationToken"/>.
+    /// </remarks>
+    public static Task RunAsync(Action<CancellationToken> action, CancellationToken cancellationToken)
+    {
+        var completionSource = new TaskCompletionSource();
+        var thread = new Thread(() =>
+        {
+            try
+            {
+                action(cancellationToken);
+                completionSource.SetResult();
+            }
+            catch (OperationCanceledException ex) when (ex.CancellationToken == cancellationToken)
+            {
+                completionSource.SetCanceled(cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                completionSource.SetException(ex);
+            }
+        });
+        thread.SetApartmentState(ApartmentState.STA);
+        thread.Start();
+        return completionSource.Task;
+    }
+}
+
---
+++ HamedStack.Assistant/Utilities/UUIDv7.cs
@@ -0,0 +1,52 @@
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
+using System.Security.Cryptography;
+
+namespace HamedStack.Assistant.Utilities;
+
+/// <summary>
+/// A class that provides functionality to generate UUID version 7.
+/// UUID version 7 is a hypothetical new version of UUIDs that includes
+/// a timestamp component as part of its value.
+/// </summary>
+public class UUIDv7
+{
+    private static readonly RandomNumberGenerator random = RandomNumberGenerator.Create();
+
+    /// <summary>
+    /// Generates a UUID version 7 byte array.
+    /// UUID version 7 includes a timestamp and random component.
+    /// </summary>
+    /// <returns>A byte array representing the generated UUID version 7.</returns>
+    public static byte[] Generate()
+    {
+        var value = new byte[16];
+        random.GetBytes(value);
+
+        var timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+        value[0] = (byte)((timestamp >> 40) & 0xFF);
+        value[1] = (byte)((timestamp >> 32) & 0xFF);
+        value[2] = (byte)((timestamp >> 24) & 0xFF);
+        value[3] = (byte)((timestamp >> 16) & 0xFF);
+        value[4] = (byte)((timestamp >> 8) & 0xFF);
+        value[5] = (byte)(timestamp & 0xFF);
+
+        value[6] = (byte)((value[6] & 0x0F) | 0x70);
+        value[8] = (byte)((value[8] & 0x3F) | 0x80);
+
+        return value;
+    }
+
+    /// <summary>
+    /// Creates a UUID version 7 string.
+    /// This string is a hexadecimal representation of the UUID byte array.
+    /// </summary>
+    /// <returns>A string representing the generated UUID version 7.</returns>
+    public static string New()
+    {
+        var uuidBytes = Generate();
+        return uuidBytes.Aggregate(string.Empty, (current, b) => current + $"{b:x2}");
+    }
+}
