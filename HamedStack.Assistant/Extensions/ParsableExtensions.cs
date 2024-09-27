6c09476e264a10561d52e4a5b886fdc3ba05aaab
hamedfathi <hamedfathi@outlook.com>
2024-09-27 18:54:52 +0200

    feat(ParsableExtensions): new extension method added.

---
+++ HamedStack.Assistant/Extensions/ParsableExtensions.cs
@@ -0,0 +1,16 @@
+﻿// ReSharper disable CheckNamespace
+// ReSharper disable UnusedMember.Global
+
+namespace HamedStack.Assistant.Extensions.ParsableExtended;
+
+#if NET7_0_OR_GREATER
+public static class ParsableExtensions
+{
+    public static T Parse<T>(this string input, IFormatProvider? formatProvider = null)
+        where T : IParsable<T>
+    {
+        return T.Parse(input, formatProvider);
+    }
+}
+#endif
+
